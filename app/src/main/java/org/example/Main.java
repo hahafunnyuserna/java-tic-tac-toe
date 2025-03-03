5efe123643596df3a22a96e199f4206665745720
hahafunnyuserna <madeforschool69@gmail.com>
2025-03-03 10:37:52 -0600

    first push
    will update today

---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ app/src/test/java/org/example/ShapesTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class ShapesTest {
+    
+    
+
+}
